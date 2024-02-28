85391d94c56ff02e4adbb338e87df468b92c555b
alexis <alexisleclair19@gmail.com>
2024-02-28 09:45:17 -0500

    add population method

--- src/main/java/org/example/populationgrowthmodel/Population.java
+++ src/main/java/org/example/populationgrowthmodel/Population.java
@@ -9,11 +9,13 @@ public class Population {
     double birthRate;
     double deathRate;
 
-    Population(String name, int initialPopulationSize, double growthRate, double birthRate,
-               double deathRate){
+    Population(String name, int initialPopulationSize, int currentPopulationSize,
+               double growthRate, double time, double birthRate, double deathRate){
         this.name = name;
         this.initialPopulationSize = initialPopulationSize;
+        this.currentPopulationSize = currentPopulationSize;
         this.growthRate = growthRate;
+        this.time = time;
         this.birthRate = birthRate;
         this.deathRate = deathRate;
 
