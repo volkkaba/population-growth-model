9dc659fb6af66820bdb83809db6c08f00e673402
boltswagen <amilrumbo@gmail.com>
2024-02-28 10:41:47 -0500

    k

--- src/main/java/org/example/populationgrowthmodel/Info.java
+++ src/main/java/org/example/populationgrowthmodel/Info.java
@@ -1,6 +1,13 @@
 package org.example.populationgrowthmodel;
 
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 public class Info extends Pane {
+    TextField tf1 = new TextField();
+    TextField tf2 = new TextField();
+
+    public void InfoBox(){
+
+    }
 }
