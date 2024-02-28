eecc33e3bbbc0435ce846c2546efe750146d7bed
boltswagen <amilrumbo@gmail.com>
2024-02-28 10:05:08 -0500

    k

--- src/main/java/org/example/populationgrowthmodel/Logistical.java
+++ src/main/java/org/example/populationgrowthmodel/Logistical.java
@@ -5,4 +5,26 @@ public class Logistical extends Population {
                double growthRate, double time, double birthRate, double deathRate){
         super(name, initialPopulationSize, currentPopulationSize, growthRate, time, birthRate, deathRate);
     }
+    // Double end is just a filler to avoid error on the return, actual code meant to be entered
+    public double ComputeLogisticalGrowth(){
+        double end = 1;
+        return end;
+    }
+    public double ComputeLogisticalSize(){
+        double end = 1;
+        return end;
+    }
+    public double ComputeDiscreteLogisticalSize(){
+        double end = 1;
+        return end;
+    }
+    public double ComputeLogisticalGrowthRate(){
+        double end = 1;
+        return end;
+    }
+    public void GenerateLogisticalPath(){
+
+    }
+
+
 }
