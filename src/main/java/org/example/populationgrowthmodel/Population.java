923d33514f987275ec33e97a3c7fad4b33269e97
alexis <alexisleclair19@gmail.com>
2024-02-28 09:31:31 -0500

    added variables

--- src/main/java/org/example/populationgrowthmodel/Population.java
+++ src/main/java/org/example/populationgrowthmodel/Population.java
@@ -3,4 +3,12 @@ package org.example.populationgrowthmodel;
 public class Population {
     String name;
     int initialPopulationSize;
+    int currentPopulationSize;
+    double growthRate;
+    double time;
+    double birthRate;
+    double deathRate;
+
+
+
 }
