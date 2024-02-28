52d04f9f205fec3f5db55442d530e923bcf360ed
boltswagen <amilrumbo@gmail.com>
2024-02-28 10:47:20 -0500

    k

--- src/main/java/org/example/populationgrowthmodel/Info.java
+++ src/main/java/org/example/populationgrowthmodel/Info.java
@@ -6,6 +6,7 @@ import javafx.scene.layout.Pane;
 public class Info extends Pane {
     TextField tf1 = new TextField();
     TextField tf2 = new TextField();
+    // These are just meant to be text not input boxes
 
     public void InfoBox(){
 
--- src/main/java/org/example/populationgrowthmodel/LogisticalGraph.java
+++ src/main/java/org/example/populationgrowthmodel/LogisticalGraph.java
@@ -1,6 +1,19 @@
 package org.example.populationgrowthmodel;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
 public class LogisticalGraph extends Pane {
+    int xAxis;
+    int yAxis;
+    TextField nameTextField = new TextField("Project Title");
+    TextField popSizeTextField = new TextField("Initial Pop. Size");
+    TextField growthRateTextField = new TextField("Growth Rate");
+    TextField birthRateTextField = new TextField("Birth Rate");
+    TextField deathRateTextField = new TextField("Death Rate");
+    TextField carryingCapacityTextField = new TextField("Carrying Capacity");
+    Button btSave = new Button("Save");
+
+
 }
