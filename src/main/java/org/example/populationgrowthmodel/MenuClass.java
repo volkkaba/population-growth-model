23786fc56f1ea1d2bc430d2f08f1709f47f5994b
Dani Rolam <dani.rolam@gmail.com>
2024-03-10 01:36:48 -0500

    added fxml, begun front end

--- src/main/java/org/example/populationgrowthmodel/MenuClass.java
+++ src/main/java/org/example/populationgrowthmodel/MenuClass.java
@@ -1,6 +1,8 @@
 package org.example.populationgrowthmodel;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -14,6 +16,8 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class MenuClass extends Application {
 
     public static void main(String[] args) {
@@ -21,38 +25,11 @@ public class MenuClass extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        // Text
-        Text welcomeText = new Text("Welcome to\nPopulation Dynamics\nSimulator");
-        Font font = Font.font("Consolas", FontWeight.BOLD, 24);
-        welcomeText.setFont(font);
-        welcomeText.setFill(Color.BLACK);
-
-        // Create HBox and display of simulator options
-        HBox hb = new HBox();
-        Pane expoPane = new Pane();
-        // SET IMAGE
-        ImageView expoImg = new ImageView(new Image("file:assets/exponential_model.jpg"));
-        expoPane.getChildren().addAll(expoImg);
-        Pane logisPane = new Pane();
-        // SET IMAGE
-        ImageView logisImg = new ImageView(new Image("file:assets/logistical_model.jpg"));
-        logisPane.getChildren().addAll(logisImg);
-        hb.getChildren().addAll(expoPane, logisPane);
-
-        // Set actions for user selection
-        expoPane.setOnMouseClicked(e -> openExpoSimulator());
-        logisPane.setOnMouseClicked(e -> openLogisSimulator());
-
-        // Add nodes to pane
-        Pane mainPane = new Pane();
-        mainPane.getChildren().addAll(welcomeText, hb);
-
-        // Change color of pane
-        mainPane.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, null, null)));
-
-        // Add pane -> scene -> stage
-        Scene scene = new Scene(mainPane, 320, 240);
+    public void start(Stage primaryStage) throws IOException {
+       
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MenuClass.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Population Dynamics Simulator");
         primaryStage.show();
