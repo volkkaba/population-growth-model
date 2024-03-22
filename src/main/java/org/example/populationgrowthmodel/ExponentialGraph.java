04b94b58ca2266059a59c759d576f844ada6ce24
Dani Rolam <dani.rolam@gmail.com>
2024-03-22 10:13:28 -0400

    exponential graph progress

--- src/main/java/org/example/populationgrowthmodel/ExponentialGraph.java
+++ src/main/java/org/example/populationgrowthmodel/ExponentialGraph.java
@@ -1,10 +1,17 @@
 package org.example.populationgrowthmodel;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-public class ExponentialGraph extends Pane {
+import java.io.IOException;
+
+public class ExponentialGraph extends Application {
     int xAxis;
     int yAxis;
     TextField nameTextField = new TextField("Project Title");
@@ -20,4 +27,16 @@ public class ExponentialGraph extends Pane {
     public void Save(){
 
     }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ExponentialGraph.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Population Dynamics Simulator");
+        primaryStage.show();
+    }
+
 }
--- src/main/java/org/example/populationgrowthmodel/FXMLMenuController.java
+++ src/main/java/org/example/populationgrowthmodel/FXMLMenuController.java
@@ -1,9 +1,14 @@
 package org.example.populationgrowthmodel;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class FXMLMenuController {
     private Stage primaryStage;
@@ -15,22 +20,22 @@ public class FXMLMenuController {
     private ImageView imageExpo, imageLogis;
 
     @FXML
-    private void imageExpoAction(MouseEvent event){
+    private void imageExpoAction(MouseEvent event) throws IOException {
         // action to open logistical FX window/pane, fxml and java controller classes need to be created first
-        /*
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("ExponentialScene.fxml"));
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ExponentialGraph.fxml"));
         Parent root = loader.load();
-        ExponentialSceneController controller = loader.getController();
-        controller.setPrimaryStage(primaryStage);
-        primaryStage.setScene(new Scene(root));
-        */
+        ExponentialGraph controller = loader.getController();
+        Scene exponentialScene = new Scene(root);
+        primaryStage.setScene(exponentialScene);
+
     }
 
     @FXML
     private void imageLogisAction(MouseEvent event){
         // action to open exponential FX window/pane, fxml and java controller classes need to be created first
         /*
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("LogisticalScene.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("LogisticalGraph.fxml"));
         Parent root = loader.load();
         LogisticalSceneController controller = loader.getController();
         controller.setPrimaryStage(primaryStage);
