f2b780df1ad37314197e033dcaacaa93a92221cb
boltswagen <amilrumbo@gmail.com>
2024-04-19 17:23:00 -0400

    designed info box but fonts not working

--- src/main/java/org/example/populationgrowthmodel/MenuClass.java
+++ src/main/java/org/example/populationgrowthmodel/MenuClass.java
@@ -5,15 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
